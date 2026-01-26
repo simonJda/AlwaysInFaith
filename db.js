d139b13c2d8086d7abd570176201bf17ef4c11e6
Simon Jarda <snjarda@gmail.com>
2026-01-26 22:25:15 +0100

    New Version for own server

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: false
 });
-
+        
 module.exports = pool;
